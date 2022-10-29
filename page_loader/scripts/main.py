5a9a731be8f322a0dabd1d721edff24d896454e0
dmedvedev <d.medvedev@velvetech.dev>
2022-10-29 20:29:18 +0700

    pass output dir with -o arg

--- page_loader/scripts/main.py
+++ page_loader/scripts/main.py
@@ -9,13 +9,14 @@ def _exit_with_error(error_message, page_url, e):
     print(error_message)
     logger.error(f'Exception while downloading {page_url}')
     logger.exception(e)
-    sys.exit()
+    sys.exit(1)
 
 
 def main(*args, **kwargs):
     parser = argparse.ArgumentParser(description='Save page to a folder')
     parser.add_argument('url', type=str, help='page url')
-    parser.add_argument('save_path', type=str, help='path to save page')
+    parser.add_argument('-o', type=str, dest='save_path',
+                        help='path to save page', required=True)
 
     args = parser.parse_args()
     try:
@@ -28,6 +29,7 @@ def main(*args, **kwargs):
         _exit_with_error("Can't write html file to disk", args.url, e)
     except Exception as e:
         _exit_with_error("Can't download page", args.url, e)
+    sys.exit(0)
 
 
 if __name__ == '__main__':
