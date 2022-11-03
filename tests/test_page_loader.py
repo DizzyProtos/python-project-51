5a8df49a9eb45a554b78d46f8fe4d9d3de153df6
dmedvedev <d.medvedev@velvetech.dev>
2022-11-03 19:27:23 +0700

    Use fixtures in tests

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -14,6 +14,41 @@ resource_tags = ['img', 'link', 'script', 'a']
 site_url = 'https://site.com/'
 
 
+@pytest.fixture
+def output_folder():
+    save_folder = f'.{os.path.sep}tmp'
+    if not os.path.isdir(save_folder):
+        os.mkdir(save_folder)
+    yield save_folder
+    shutil.rmtree(save_folder)
+
+
+@pytest.fixture
+def html_page():
+    html_page_path = 'tests/fixtures/page.html'
+    with open(html_page_path, 'r') as f:
+        page_text = f.read()
+    return page_text
+
+
+@pytest.fixture
+def resources():
+    with open('tests/fixtures/resources_urls.json', 'r') as f:
+        resources_urls = json.load(f)
+    return resources_urls
+
+
+@pytest.fixture
+def mocker(html_page, resources):
+    local_file_uri = f"file:///{os.getcwd().replace(os.path.sep, '/')}"
+    with requests_mock.Mocker(real_http=False) as mock:
+        mock.get(site_url, text=html_page)
+        for mock_url, file_path in resources.items():            
+            file_uri = f'{local_file_uri}/{file_path}'
+            mock.get(mock_url, body=file_uri)
+        yield mock
+
+
 def itterate_resources(html_text):
     website = soup(html_text)
     for resource in website.find_all(resource_tags):
@@ -40,45 +75,23 @@ def check_if_resources_exist(html_text, save_folder):
     return count
 
 
-def test_page_loader():
-    htmp_page_path = 'tests/fixtures/page.html'
-    with open(htmp_page_path, 'r') as f:
-        page_text = f.read()
-
-    save_folder = f'.{os.path.sep}tmp'
-    if not os.path.isdir(save_folder):
-        os.mkdir(save_folder)
-
-    with open('tests/fixtures/resources_urls.json', 'r') as f:
-        resources_urls = json.load(f)
-
-    local_file_uri = f"file:///{os.getcwd().replace(os.path.sep, '/')}"
-    with requests_mock.Mocker(real_http=True) as m:
-        m.get(site_url, text=page_text)
-        for mock_url, file_path in resources_urls.items():            
-            file_uri = f'{local_file_uri}/{file_path}'
-            m.get(mock_url, body=file_uri)
-
-        output_file = download(site_url, save_folder)
+def test_page_loader(mocker, output_folder):
+    output_file = download(site_url, output_folder)
 
     with open(output_file, 'r') as f:
         downloaded_text = f.read()
 
-    assert check_if_resources_exist(downloaded_text, save_folder) == 4
+    assert check_if_resources_exist(downloaded_text, output_folder) == 4
     assert filecmp.cmp(output_file, './tests/fixtures/correct_page.html')
 
     os.remove(output_file)
-    shutil.rmtree(save_folder)
 
 
-def test_connection_error():
+def test_connection_error(output_folder):
     with pytest.raises(ConnectionError):
-        if not os.path.isdir('./connection_error/'):
-            os.mkdir('./connection_error/')
-        download('http://badqwref23site.com', './connection_error/')
-    shutil.rmtree('./connection_error/')
+        download('http://badqwref23site.com', output_folder)
 
 
-def test_non_existing_path():
+def test_non_existing_path(output_folder):
     with pytest.raises(IOError):
-        download('http://google.com', './ioerror/ioerror/ioerror/')
+        download('http://google.com', output_folder)
